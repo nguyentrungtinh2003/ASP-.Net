9d363cb23f6b99058badb3d5afd06f24811bc178
nguyentrungtinh <trungtinhn300@gmail.com>
2024-06-22 11:22:32 +0700

    Upload Buoi 4 ASP

---
+++ Buổi 4-UploadFile/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Buổi_4_UploadFile.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", file.FileName);
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                    }
+                }
+                ViewBag.Message = "Files uploaded successfully.";
+            }
+            else
+            {
+                ViewBag.Message = "No files selected.";
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ Buổi 4-UploadFile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buổi_4_UploadFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
