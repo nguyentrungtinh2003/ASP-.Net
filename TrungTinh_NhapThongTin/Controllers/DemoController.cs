0202a6e25e13e048680188b9c7db00bfdbb47fd7
nguyentrungtinh <trungtinhn300@gmail.com>
2024-06-22 11:23:55 +0700

    Upload Buoi 1 Nhap Thong Tin

---
+++ TrungTinh_NhapThongTin/Controllers/DemoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrungTinh_NhapThongTin.Models;
+
+namespace TrungTinh_NhapThongTin.Controllers
+{
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public IActionResult NhapThongTin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult KetQua(string name, int birthYear)
+        {
+            ViewBag.Name = name;
+            ViewBag.BirthYear = birthYear;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(CalculatorModel model)
+        {
+            switch (model.Operation)
+            {
+                case "Cộng":
+                    model.Result = model.Number1 + model.Number2;
+                    break;
+                case "Trừ":
+                    model.Result = model.Number1 - model.Number2;
+                    break;
+                case "Nhân":
+                    model.Result = model.Number1 * model.Number2;
+                    break;
+                case "Chia":
+                    model.Result = model.Number1 / model.Number2;
+                    break;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ TrungTinh_NhapThongTin/Models/CalculatorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TrungTinh_NhapThongTin.Models
+{
+    public class CalculatorModel
+    {
+
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Operation { get; set; }
+        public double Result { get; set; }
+    }
+}
