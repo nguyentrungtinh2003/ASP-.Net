b0f5c25d279bd8d4cbd43359b291ee1021ab77fb
nguyentrungtinh <trungtinhn300@gmail.com>
2024-06-28 08:55:04 +0700

    Upload Buổi 5 ASP

---
+++ Buổi 5-ASP/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buổi_5_ASP.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Buổi 5-ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buổi_5_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
