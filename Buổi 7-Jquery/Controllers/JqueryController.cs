6b3ea7aa0787f2f0e734c86b4421d5f465d241eb
nguyentrungtinh <trungtinhn300@gmail.com>
2024-07-06 09:53:26 +0700

    Upload Buổi 7 Jquery

---
+++ Buổi 7-Jquery/Controllers/JqueryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buổi_7_Jquery.Controllers
+{
+    public class JqueryController : Controller
+    {
+       
+
+        public IActionResult RegisterMember()
+        {
+            return View();
+        }
+    }
+}
