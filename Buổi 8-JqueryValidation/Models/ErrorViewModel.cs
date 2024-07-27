45f2a7e19361d76b73f42640bdd7b3b2657f3888
nguyentrungtinh <trungtinhn300@gmail.com>
2024-07-27 10:03:57 +0700

    Upload Buổi 8-Jquery

---
+++ Buổi 8-JqueryValidation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buổi_8_JqueryValidation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
