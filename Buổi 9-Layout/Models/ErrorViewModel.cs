ee089bc224f798adc40808ea8913e769f853f738
nguyentrungtinh <trungtinhn300@gmail.com>
2024-07-27 10:05:04 +0700

    Upload Buổi 9-Layout

---
+++ Buổi 9-Layout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buổi_9_Layout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
